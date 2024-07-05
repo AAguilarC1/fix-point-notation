02090bb357c5e0c1756b62eb13f4889d27163d10
Abel_personal <=>
2024-07-05 18:34:32 +0100

    Refactor: Order of my macros for Q notaton

--- include/fix_point.h
+++ include/fix_point.h
@@ -6,19 +6,22 @@
 #define Q_FORMAT_15     15
 #define Q_FORMAT_31     31
 
-#define Q_FORMAT_CUSTOM 15
+#define Q_FORMAT_CUSTOM 16
 
 #define Q_FORMAT Q_FORMAT_CUSTOM
 
 #if     Q_FORMAT == Q_FORMAT_7
 typedef int8_t q7_t;
 typedef q7_t q_t;
+#define Q_FORM_INT_BITS (sizeof(q_t) << 1)
 #elif   Q_FORMAT == Q_FORMAT_15
 typedef int16_t q15_t;
 typedef q15_t q_t;
+#define Q_FORM_INT_BITS (sizeof(q_t) << 2)
 #elif   Q_FORMAT == Q_FORMAT_31
 typedef int32_t q31_t;
 typedef q31_t q_t;
+#define Q_FORM_INT_BITS (sizeof(q_t) << 3)
 #elif   Q_FORMAT == Q_FORMAT_CUSTOM
 typedef int32_t q_t;
 #else
@@ -26,7 +29,7 @@ typedef int32_t q_t;
 #endif
 
 #define FRACTIONAL_BITS (Q_FORMAT)
-#define GET_FORMAT (FRACTIONAL_BITS)
+#define INT_BITS (Q_FORM_INT_BITS - FRACTIONAL_BITS)
 
 #define INT_TO_Q(x) ((q_t)((x) << FRACTIONAL_BITS))
 #define Q_TO_INT(x) ((int32_t)(x) >> FRACTIONAL_BITS)
