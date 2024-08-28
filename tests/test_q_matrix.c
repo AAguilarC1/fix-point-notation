469aa6d4631b2ac4a1d85e77221f80e74b3f1397
Abel_personal <=>
2024-08-28 12:02:55 +0100

    Initialized the test for the matrices

---
+++ tests/test_q_matrix.c
@@ -0,0 +1,4 @@
+#include "test_q_matrix.h"
+//TODO: Implement tests for q_matrix.c
+
+void add_matrix_tests(CU_pSuite suite);
---
+++ tests/test_q_matrix.h
@@ -0,0 +1,9 @@
+#ifndef TEST_Q_MATRIX_H
+#define TEST_Q_MATRIX_H
+#include "CUnit/Basic.h"
+#include "../include/fix_point_matrix.h"
+//TODO: Implement the tests for the matrix of fixed point numbers
+
+void add_matrix_tests(CU_pSuite suite);
+
+#endif // TEST_Q_MATRIX_H
